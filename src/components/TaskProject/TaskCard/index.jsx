fd10c8c8158e8093b58d9f3bfc3f0bd1ce477ad6
Garnik Hovsepyan <garoh2291@gmail.com>
2022-06-22 11:47:34 +0400

    changes

--- src/components/TaskProject/TaskCard/index.jsx
+++ src/components/TaskProject/TaskCard/index.jsx
@@ -9,18 +9,29 @@ export const TaskCard = memo(({ taskInfo }) => {
   const { title, created_at, description, owner, priority } = taskInfo;
   const { TotalDays } = useCalculateDate(created_at);
   const { name, surname } = useGetTaskAssigneeDetails(owner);
-
+  console.log(priority);
   return (
     <div className="task_card">
       <h6 className="task_header">{title}</h6>
       <p className="task_description">{description}</p>
       <div className="task_details">
-        <i
-          className={`${
-            TotalDays < 7 ? "short-time" : "long-time"
-          } time-opened-icons bx bx-dots-horizontal-rounded`}
-          title={`${TotalDays} Days`}
-        ></i>
+        <div className="time_and_priority">
+          <i
+            className={`${
+              TotalDays < 7 ? "short-time" : "long-time"
+            } time-opened-icons bx bx-dots-horizontal-rounded`}
+            title={`${TotalDays} Days`}
+          ></i>
+          <i
+            className={
+              priority === "minor"
+                ? "minor_priority priority_icons bx bxs-chevrons-down"
+                : "major_priority priority_icons bx bxs-chevrons-up"
+            }
+            title={priority}
+          ></i>
+        </div>
+
         <div className="assignee_details">
           <p title={name ? `${name} ${surname}` : "Unassigned"}>
             {name ? `${name[0]} ${surname[0]}` : "U"}
